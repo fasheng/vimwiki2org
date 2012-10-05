c0e9ebd4c69d89dbe38d51bed1652a8907810c9c
Xu FaSheng <xufasheng2006@gmail.com>
2012-10-05 23:09:11 +0800

    * could dispatch options * write pod document for the program

--- vimwiki2org.pl
+++ vimwiki2org.pl
@@ -19,12 +19,16 @@
 
 # Commentary:
 #
-# This is a simple tool to convert vimwiki files to org-mode file, and the
-# usage is simple:
+# This is a simple tool to convert vimwiki files to emacsorg-mode file,
+# and the usage is simple:
 # perl vimwiki2org.pl index.wiki > vimwiki.org
+# perl vimwiki2org.pl -t tag1:tag2 -l log.txt -- index.wiki > vimwiki.org
 #
-# more information to see help: TODO
+# more options to see help
 # perl vimwiki2org.pl --help
+#
+# more conversion rules to see man page
+# perl vimwiki2org.pl --man
 #
 
 # Change Log:
@@ -58,28 +62,24 @@ use List::Util qw/first/;
 use File::Basename;
 use File::Spec;
 use Cwd 'abs_path';
-# TODO, getopt
+use Getopt::Long;
+use Pod::Usage;
 
-# options TODO
-my $org_file_tags = "vimwiki";  # add org file tags
-my $ignore_lonely_header = 1;   # TODO
-my $log_file = "/tmp/vimwiki2org.log";
-my $log_fh;
-my $default_src_type;
+## global variables
+my $program_name="vimwiki2org.pl";
+my $version_msg = "$program_name version 0.1";
 
-# other variables
-my $vimwiki_ext = '.wiki';
 my @dispatched_files;
 my @open_error_files;
-my $org_commit = "#";           # TODO
+my $org_comment = "#";           # TODO
 
 # links in vimwiki
 # such as '[[file]] [[file|name]]'
 my $link_regexp = '\[\[(.*?)\]\]';
 
-# commit and placeholder of vimwiki, which all start with "%"
-# such as "%title", "%% commit"
-my $commit_regexp = '^\s*%';
+# comment and placeholder of vimwiki, which all start with "%"
+# such as "%title", "%% comment"
+my $comment_regexp = '^\s*%';
 
 # headers in vimwiki
 # such as "=== header ==="
@@ -92,25 +92,57 @@ my $list_regexp = '^(\s*)([#*-]) (.*)$';
 # plain text in vimwiki
 my $plain_regexp = '^(\s*)(.*)$';
 
-# source block in vimwiki
+# source(preformat) block in vimwiki
 # such as '{{{', '{{{sh', '}}}'
-my $src_block_begin_with_type_regexp = '^\s*{{{\s*\S+$';
+my $src_block_begin_with_type_regexp = '^\s*{{{\s*\S+.*$';
 my $src_block_begin_no_type_regexp = '^\s*{{{\s*$';
 my $src_block_end_regexp = '^\s*}}}\s*$';
 
+# other
+my $log_fh;
+
+# options
+my $org_file_tags = "vimwiki";  # add org file tags
+my $log_file = "/tmp/vimwiki2org.log";
+my $vimwiki_ext = '.wiki';
+my $ignore_lonely_header = 1;
+my $untyped_preformat_block_convert_type = undef;
+
 ## main loop
+my $man = 0;
+my $help = 0;
+my $version = 0;
+GetOptions(
+    'file-tags|t:s'       => \$org_file_tags,
+    'no-file-tags|no-t'   => sub{ $org_file_tags='' },
+    'log-file|l:s'        => \$log_file,
+    'no-log-file|no-l:s'  => sub{ $log_file='' },
+    'vimwiki-ext|e:s'     => \$vimwiki_ext,
+    'ignore-lonely-header|i!'    => \$ignore_lonely_header,
+    'untyped-preformat-block-convert-type|u:s' => \$untyped_preformat_block_convert_type,
+    'help|h'    => \$help,
+    'man|m'     => \$man,
+    'version|v' => \$version
+) or pod2usage(1);
+pod2usage(1) if $help;
+pod2usage(-verbose => 2) if $man;
+if ($version) {
+    say $version_msg;
+    exit;
+}
+
 &open_log();
-if (defined $org_file_tags) {
-    say $org_commit, '+FILETAGS: :', $org_file_tags, ":";
+if ($org_file_tags) {
+    say $org_comment, '+FILETAGS: :', $org_file_tags, ":\n";
 }
 my $diary_index_file;
 foreach (@ARGV) {
     # the vimwiki index file
     my $index_file = $_;
     $index_file = File::Spec->canonpath($index_file);
-    my $vimwiki_base_dir = dirname $index_file;
 
     # the vimwiki diary index file, this file will dispatched at end
+    my $vimwiki_base_dir = dirname $index_file;
     $diary_index_file = &build_path($vimwiki_base_dir, "diary/diary.wiki");
 
     &open_and_dispatch($index_file);
@@ -133,12 +165,14 @@ sub build_path {
 }
 
 sub open_log {
+    return unless $log_file;    # TODO
     eval {open $log_fh, ">", $log_file};
     warn $@ if ($@);
     &append_log("# MESSAGES");
 }
 
 sub append_log {
+    return unless defined $log_fh;
     my $msg = shift;
     say {$log_fh} $msg if (fileno $log_fh);
 }
@@ -222,8 +256,8 @@ sub open_and_dispatch {
     foreach(@content) {
         # my $exist_link_in_cur_line = 0;
         given ($_) {
-            # commit and placeholder of vimwiki, which all start with "%"
-            when (/$commit_regexp/) {
+            # comment and placeholder of vimwiki, which all start with "%"
+            when (/$comment_regexp/) {
                 say "# ", $_;
             }
 
@@ -245,7 +279,7 @@ sub open_and_dispatch {
                 if ($ignore_lonely_header) {
                     # if there is only one header, ignore it
                     if ($header_count <= 1) {
-                        say $org_commit, $_;
+                        say $org_comment, $_;
                         break;
                     }
                 }
@@ -372,9 +406,11 @@ sub convert_link_format {
         my $link_content = "";
         my $link_des = "";
         if (index($_, "|") >= 0) {
+            # exist description in vimwiki link, split it
             $link_content = s/\|.*//r;
             $link_des = s/.*\|//r;
         } else {
+            # there is no description
             $link_content = $_;
         }
         if (!length $link_content) {next;}
@@ -410,11 +446,9 @@ sub collect_links {
         my $link_content = "";
         my $link_des = "";
         if (index($_, "|") >= 0) {
-            # exist description in vimwiki link, split it
             $link_content = s/\|.*//r;
             $link_des = s/.*\|//r;
         } else {
-            # there is no description
             $link_content = $_;
         }
         if (!length $link_content) {next;}
@@ -443,13 +477,188 @@ sub expand_collected_links {
             &append_log("find diary index file as a link: " . $_);
         }
     }
-    # say foreach (@$collected_links_ref);
 
     # empty collected links
     @$collected_links_ref = ();
 }
 
-# TODO
-sub help {
-    # how to check log
-}
+__END__
+
+=head1 NAME
+
+vimwiki2org.pl - a simple tool to convert vimwiki to emacs org-mode
+
+=head1 SYNOPSIS
+
+vimwiki2org.pl index.wiki [file ...]
+
+vimwiki2org.pl [options] -- index.wiki [file ...]
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-t>, B<--file-tags>=tag1:tag2, B<--no-t>, B<--no-file-tags>
+
+set the org-mode's file tags(#+FILETAGS), if is empty or
+use B<--no-file-tags> will not insert file tags
+(default value is: "vimwiki")
+
+=item B<-l>, B<--log-file>=log.txt, B<--no-l>, B<--no-log-file>
+
+set the log file, if is empty or use B<--no-log-file> will not write log
+(default value is: "/tmp/vimwiki2org.log")
+
+=item B<-e>, B<--vimwiki-ext>=.wiki
+
+set the default vimwiki's file extension name
+(default value is: ".wiki")
+
+=item B<-i>, B<--ignore-lonely-header>, B<--no-i>, B<--no-ignore-lonely-header>
+
+set if ignore the only one header in a vimwiki file, because we have
+set its filename as a parent org-mode headline
+(default option is: B<--ignore-lonely-header>)
+
+=item B<-u>, B<--untyped-preformat-block-convert-type>=perl
+
+set the untyped preformat block's type after converting, vimwiki's preformat
+block is code which in 'B<{{{...}}}>', some with a type such as
+'B<{{{sh...>', another without a type such as 'B<{{{...>', default, a
+typed preformat code will convert to as org source block, such as
+'B<#+<begin_src sh>', an untyped preformat code will convert to as org
+example block, such as 'B<#+begin_example>', if you use
+B<--untyped-preformat-block-convert-type>, the untyped preformat block will
+convert to as org source block with your defined type, too
+(default value is: <undef>)
+
+=item B<-h>, B<--help>
+
+print a brief help message and exits
+
+=item B<-m>, B<--man>
+
+prints the manual page and exits
+
+=item B<-v>, B<--version>
+
+prints the version information and exits
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will read the given input vimwiki file(s) and convert to
+org-mode and output, here is the conversion rules:
+
+=over 8
+
+=item * B<comment> and B<placeholder> in vimwiki, which start with 'B<%>'
+
+all will be treat as org-mode's comment line which start with 'B<#>',
+B<for example>:
+
+'B<%title>' =>'B<#%title>', 'B<%% comment>' => 'B<#%% comment>'
+
+=item * B<headers> in vimwiki, which surround with 'B<=>'
+
+will be treat as org-mode's headline, and the headline level should be compute
+as followed steps:
+first, if you enable option 'B<--ignore-lonely-header>'(default is enabled),
+and there is only one header in the file, just ignore the header,
+and comment the line, because a file always has a overall parent headline
+which named as the file name, another overall parent headline is redundant;
+second, find the minimum header level in file, and set it as
+the start level, B<for example>, the minimus header level is 3,
+if not think of its parent level, it will be
+convert to a org headline as level 1,
+
+'B<=== header ===>'  =>  'B<* header>'
+
+=item * B<list> item in vimwiki, which start with 'B<->' 'B<*>' 'B<#>'
+
+the first level list item under a org-mode headline, will be convert to
+headline, just like header in vimwiki,
+and the TODO tag will convert to org-mode's TODO state,
+B<for example>:
+
+'B<- list item>'      =>  'B<* list item>',
+
+'B<* [ ] list item>'  =>  'B<* TODO list item>',
+
+'B<# [X] list item>'  =>  'B<* DONE list item>'
+
+the sub level list item will be convert to standard org-mode plain list,
+which start with 'B<->',
+B<for example>:
+
+'B<- list item>'      =>  'B<- list item>'
+
+'B<* [ ] list item>'  =>  'B<- [ ] list item>'
+
+=item * B<links> in vimwiki, which surround with 'B<[[>' 'B<]]>'
+
+links will be treated as two kinds: file link(link to other vimwiki file),
+and url link(http://, https://, ftp:// and so on).
+first, convert the links to org-mode format, B<for example>:
+
+'B<[[path/file]]>'  =>  'B<[[file]]>'
+
+'B<[[file|des]]>'   =>  'B<[[file][des]]>'
+
+'B<[[http://url|des]]>'  =>  'B<[[http://url][des]]>'
+
+then, collect all the file links under the current org-mode headline,
+and before next headline, expand these links, for each file, if
+not dispatched yet, and is not the diary index file, will be append as a child
+headline, named with the file name
+
+=item * B<preformat>(B<source>) block in vimwiki, which surround with 'B<{{{>' 'B<}}}>'
+
+preformat block will be convert to example or source block in org-mode,
+if the preformat block with a type, such as 'perl' or 'python', it will
+convert to source block, if no, will convert to a example block, but if
+you use the option 'B<--untyped-preformat-block-convert-type>', the
+untyped preformat block will convert to source block with your defined type,
+B<for example>:
+
+'B<{{{...}}}>'  =>  'B<#+begin_example...#+end_example>'
+'B<{{{perl...}}}>'  =>  'B<#+begin_src perl...#+end_src>'
+'B<{{{class="brush: sh...}}}">'  =>  'B<#+begin_src sh...#+end_src>'
+
+and all the text under the prefomat block will be treat as plain text
+
+=item * B<plain> text in vimwiki
+
+just output without change
+
+=item * B<multiple> vimwiki files as options
+
+each file will be append as a level 1 org-mode headline, named with the file name,
+unless the later file is dispatched as a link in the earlier file
+
+=item * B<diary> index file in vimwiki
+
+each file in options should has a diary index file, default is 'B<diary/diary.wiki>' relative to the file's path, it will be dispatched after the file,
+as a level 1 org-mode headline
+
+=back
+
+=head1 EXAMPLES
+
+perl vimwiki2org.pl index.wiki > vimwiki.org
+
+perl vimwiki2org.pl -t tag1:tag2 -l log.txt -- index.wiki > vimwiki.org
+
+=head1 AUTHOR
+
+Written by Xu FaSheng.
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3, or
+(at your option) any later version.
+
+=cut
